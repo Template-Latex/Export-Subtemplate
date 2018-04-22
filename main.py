ba80483c18f1641ce4749859df22f5451dd56e69
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2018-04-22 04:53:54 +0100

    Se extiende conversión a controles

--- main.py
+++ main.py
@@ -646,7 +646,8 @@ class CreateVersion(object):
                                                      mainroot=self._getconfig('MAIN_ROOT'),
                                                      informeroot=self._getconfig('INFORME_ROOT'),
                                                      statsroot=self._getconfig('STATS_ROOT'))
-                        except:
+                        except Exception, e:
+                            logging.exception('Error al generar controles')
                             clear_dict(RELEASES[REL_INFORME], 'FILES')
                             clear_dict(RELEASES[REL_AUXILIAR], 'FILES')
                             clear_dict(RELEASES[REL_CONTROLES], 'FILES')
@@ -661,7 +662,8 @@ class CreateVersion(object):
                                      mainroot=self._getconfig('MAIN_ROOT'),
                                      statsroot=self._getconfig('STATS_ROOT'),
                                      backtoroot=True)
-                        except:
+                        except Exception, e:
+                            logging.exception('Error al generar cv')
                             clear_dict(RELEASES[REL_PROFESSIONALCV], 'FILES')
                     else:
                         raise Exception('ERROR: ID INCORRECTO')
